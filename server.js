97182b09e276874c0fe3be518d441f8e93d2d122
Robbie Jaeger <jaeger.rob@gmail.com>
2019-09-30 15:12:27 -0600

    Add cors and news data

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const newsData = require('./news');
 
 app.set('port', process.env.PORT || 3001);
-
+app.use(cors());
 
 app.get('/', (request, response) => {
   response.status(200).json({"hello": "world", "projectSpec": "https://frontend.turing.io/projects/whats-new.html"});
 });
 
 app.get('/api/v1/news', (request, response) => {
-
+  response.status(200).json(newsData);
 });
 
 app.listen(app.get('port'), () => {
